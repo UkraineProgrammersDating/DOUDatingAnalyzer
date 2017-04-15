9431347205f0fa794cf2a9d3b0e1285797126144
ReenExe <ipourri@gmail.com>
2017-04-16 00:38:34 +0300

    for analyze more topics

--- src/AppBundle/Command/DatingAnalyzeCommand.php
+++ src/AppBundle/Command/DatingAnalyzeCommand.php
@@ -17,52 +17,55 @@ class DatingAnalyzeCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $crawlerService = $this
-            ->getContainer()
-            ->get('dating.crawler');
+        $topics = ['https://dou.ua/forums/topic/16490/'];
+        foreach ($topics as $topic) {
+            $crawlerService = $this
+                ->getContainer()
+                ->get('dating.crawler');
 
-        $topicCrawler = $crawlerService->parse('https://dou.ua/forums/topic/16490/');
+            $topicCrawler = $crawlerService->parse($topic);
 
-        $output->writeln($topicCrawler->filter('title')->html());
+            $output->writeln($topicCrawler->filter('title')->html());
 
-        $allProfiles = $topicCrawler->filter('a.avatar')->each(function (Crawler $profile) {
-            return [
-                'name' => trim($profile->text()),
-                'url' => $profile->attr('href'),
-            ];
-        });
-
-        $count = count($allProfiles);
-        $output->writeln("<info>Found all profiles: $count</info>");
-
-        $profiles = array_column($allProfiles, null, 'url');
-        $count = count($profiles);
-        $output->writeln("<info>Found unique profiles: $count</info>");
-
-        $socialProfiles = [];
-        foreach ($profiles as $profile) {
-            $profileCrawler = $crawlerService->parse($profile['url']);
-
-            $socials = $profileCrawler->filter('a.verified')->each(function (Crawler $crawler) {
-                return $crawler->attr('href');
+            $allProfiles = $topicCrawler->filter('a.avatar')->each(function (Crawler $profile) {
+                return [
+                    'name' => trim($profile->text()),
+                    'url' => $profile->attr('href'),
+                ];
             });
 
-            foreach ($socials as $url) {
-                if (strpos($url, 'vk.com')) {
-                    $socialProfiles[] = $profile;
+            $count = count($allProfiles);
+            $output->writeln("<info>Found all profiles: $count</info>");
+
+            $profiles = array_column($allProfiles, null, 'url');
+            $count = count($profiles);
+            $output->writeln("<info>Found unique profiles: $count</info>");
+
+            $socialProfiles = [];
+            foreach ($profiles as $profile) {
+                $profileCrawler = $crawlerService->parse($profile['url']);
+
+                $socials = $profileCrawler->filter('a.verified')->each(function (Crawler $crawler) {
+                    return $crawler->attr('href');
+                });
+
+                foreach ($socials as $url) {
+                    if (strpos($url, 'vk.com')) {
+                        $socialProfiles[] = $profile;
+                    }
                 }
             }
+
+            $count = count($socialProfiles);
+            $output->writeln("<info>Found social profiles: $count</info>");
+
+            $table = new Table($output);
+            $table
+                ->setHeaders(['name', 'url'])
+                ->setRows($socialProfiles);
+            $table->render();
+
+            $output->writeln('<info>Complete</info>');
         }
-
-        $count = count($socialProfiles);
-        $output->writeln("<info>Found social profiles: $count</info>");
-
-        $table = new Table($output);
-        $table
-            ->setHeaders(['name', 'url'])
-            ->setRows($socialProfiles);
-        $table->render();
-
-        $output->writeln('<info>Complete</info>');
     }
 }

c320c0deaba00a3742aeb923d984dab971d32642
ReenExe <ipourri@gmail.com>
2017-04-15 23:58:26 +0300

    Знайти всіх коментаторів в яких є соціальний профіль VK #4

--- src/AppBundle/Command/DatingAnalyzeCommand.php
+++ src/AppBundle/Command/DatingAnalyzeCommand.php
@@ -16,14 +16,15 @@ class DatingAnalyzeCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $crawler = $this
+        $crawlerService = $this
             ->getContainer()
-            ->get('dating.crawler')
-            ->parse('https://dou.ua/forums/topic/16490/');
+            ->get('dating.crawler');
 
-        $output->writeln($crawler->filter('title')->html());
+        $topicCrawler = $crawlerService->parse('https://dou.ua/forums/topic/16490/');
 
-        $allProfiles = $crawler->filter('a.avatar')->each(function (Crawler $profile) {
+        $output->writeln($topicCrawler->filter('title')->html());
+
+        $allProfiles = $topicCrawler->filter('a.avatar')->each(function (Crawler $profile) {
             return [
                 'name' => trim($profile->text()),
                 'url' => $profile->attr('href'),
@@ -36,6 +37,24 @@ class DatingAnalyzeCommand extends ContainerAwareCommand
         $profiles = array_column($allProfiles, null, 'url');
         $count = count($profiles);
         $output->writeln("<info>Found unique profiles: $count</info>");
+
+        $socialProfiles = [];
+        foreach ($profiles as $profile) {
+            $profileCrawler = $crawlerService->parse($profile['url']);
+
+            $socials = $profileCrawler->filter('a.verified')->each(function (Crawler $crawler) {
+                return $crawler->attr('href');
+            });
+
+            foreach ($socials as $url) {
+                if (strpos($url, 'vk.com')) {
+                    $socialProfiles[] = $profile;
+                }
+            }
+        }
+
+        $count = count($socialProfiles);
+        $output->writeln("<info>Found social profiles: $count</info>");
 
         $output->writeln('<info>Complete</info>');
     }
